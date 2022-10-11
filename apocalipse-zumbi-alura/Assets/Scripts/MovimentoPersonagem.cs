13935632c569a98e1acdb8a7ab2ea4c12a556ded
Jerson <jersonseling@yahoo.com.br>
2022-10-10 21:40:29 -0300

    correções

--- apocalipse-zumbi-alura/Assets/Scripts/MovimentoPersonagem.cs
+++ apocalipse-zumbi-alura/Assets/Scripts/MovimentoPersonagem.cs
@@ -32,7 +32,17 @@ public class MovimentoPersonagem : MonoBehaviour
     {
         meurigidbody.constraints = RigidbodyConstraints.None;
         meurigidbody.velocity = Vector3.zero;
-        meurigidbody.isKinematic = false;
+        meurigidbody.isKinematic = true;
         GetComponent<CapsuleCollider>().enabled = false;
+
+       // StartCoroutine(SumirCorpo());
     }
+
+    //IEnumerator SumirCorpo()
+    //{
+    //    yield return new WaitForSeconds(0.5f);
+        //se o jogo estiver pausado como no game over, deve-se usar
+        //o WaitForSecondsRealtime
+    //    meurigidbody.velocity = new Vector3(0, -100f, 0);
+    //}
 }
